5148bf6783a5aefbb0f25ef81abaff60f0a9272f
mdr-manish <151599906+mdr-manish@users.noreply.github.com>
2025-01-04 19:30:42 +0900

    Database Setup

---
+++ drizzle.config.js
@@ -0,0 +1,9 @@
+/**  @type {import ("drizzle-kit").Config} */
+
+export default {
+  schema: "./configs/schema.js",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: "postgresql://ai-short-video-generator_owner:GbQ1yhWMqfY8@ep-young-dream-a5vjx07u.us-east-2.aws.neon.tech/ai-short-video-generator?sslmode=requ",
+  },
+};
